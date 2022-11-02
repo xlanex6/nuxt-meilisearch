e13b75516427803959bacd2550d19358c1cfb4a0
xlanex6 <alexduval71@gmail.com>
2022-11-02 15:24:48 +0100

    chore: update all dependencies

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -9,9 +9,7 @@ export default defineNuxtConfig({
     hostUrl: 'http://0.0.0.0:7700',
     readApiKey: '<your_public_key>',
     writeApiKey: '<your_secret_key>',
-    serverSideUsage: true,
-    instantSearch: {
-      theme: 'algolia'
-    }
+    serverSideUsage: false,
+    instantSearch: false
   }
 })
