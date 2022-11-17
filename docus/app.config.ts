263f4041abd1b57e4e9b68a48c75dd7d9f13a99c
xlanex6 <alexduval71@gmail.com>
2022-11-17 20:29:05 +0100

    Add Docus

---
+++ docus/app.config.ts
@@ -0,0 +1,45 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nuxt Meilisearch',
+    description: 'Full integration of Meilisearch via Nuxt module',
+    url: 'https://nuxt-meilisearch-docs.netlify.app',
+    image: '/preview.png',
+    socials: {
+      twitter: '@xlanex6',
+      github: 'xlanex6/nuxt-meilisearch'
+    },
+    github: {
+      root: 'content',
+      edit: true,
+      contributors: false
+    },
+    aside: {
+      level: 2,
+      filter: []
+    },
+    header: {
+      title: false,
+      logo: false,
+      showLinkIcon: false
+    },
+    footer: {
+      credits: {
+        icon: 'IconDocus',
+        text: 'Powered by Docus',
+        href: 'https://docus.com'
+      },
+      icons: [
+        {
+          label: 'NuxtJS',
+          href: 'https://nuxtjs.org',
+          component: 'IconNuxtLabs'
+        },
+        {
+          label: 'Vue Telescope',
+          href: 'https://vuetelescope.com',
+          component: 'IconVueTelescope'
+        }
+      ]
+    }
+  }
+})
