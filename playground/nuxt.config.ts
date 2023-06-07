65a87ac3099a5525e6510fe74203ab570deb32ca
xlanex6 <alexduval71@gmail.com>
2023-06-07 16:07:37 +0200

    use meilisearch cloud as demo

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -6,8 +6,8 @@ export default defineNuxtConfig({
     NuxtMeilisearch
   ],
   meilisearch: {
-    hostUrl: 'http://0.0.0.0:7700',
-    searchApiKey: '<your_public_key>',
+    hostUrl: 'https://ms-15a4f248a340-186.fra.meilisearch.io',
+    searchApiKey: 'e88fb878569817bdba1d0093cebd41850ce07a44ee457f11fbde9411b1d24cd5',
     instantSearch: {
       theme: 'satellite'
     },
