e47791712e53436b7d4a717cb2d589ee0d6a85b2
xlanex6 <alexduval71@gmail.com>
2023-10-14 15:53:54 +0200

    More control on Meilisearch Server Side

--- src/runtime/server/utils/meilisearch.ts
+++ src/runtime/server/utils/meilisearch.ts
@@ -1,9 +1,10 @@
 import { useRuntimeConfig } from '#imports'
 import { MeiliSearch } from 'meilisearch'
 
-const { serverMeilisearchClient: { hostUrl, adminApiKey } } = useRuntimeConfig()
+const { serverMeilisearchClient: { hostUrl, adminApiKey, meilisearchConfig } } = useRuntimeConfig()
 
 export const $meilisearch = new MeiliSearch({
   host: hostUrl,
-  apiKey: adminApiKey
+  apiKey: adminApiKey,
+  meilisearchConfig
 })
