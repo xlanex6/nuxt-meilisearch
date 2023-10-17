a8a2716050dd9ff43281320b06369ad97f764501
xlanex6 <alexduval71@gmail.com>
2023-10-18 00:55:31 +0200

    Better playground exemple

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -8,7 +8,9 @@ export default defineNuxtConfig({
   meilisearch: {
     hostUrl: 'http://localhost:7700',
     searchApiKey: 'c0fe8e1b418fe186343ba29b6d6c0c2602a03ba6327668e4e5fb64a1c8b4031b',
-    instantSearch: false,
+    instantSearch: {
+      theme: 'satellite'
+    },
     serverSideUsage: false,
     adminApiKey: '60aa0d273dce7d47e83c574dfea5559eadcc9e989f7226d8edddd58faf2ab59d'
   }
