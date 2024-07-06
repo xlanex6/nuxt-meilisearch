97a0abbed3e2d87e35b39351ed9aeafc2f868a90
xlanex6 <alexduval71@gmail.com>
2024-07-06 19:28:12 +0200

    [chore] update package

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     NuxtMeilisearch,
     '@nuxt/ui',
   ],
+
   meilisearch: {
     hostUrl: 'http://localhost:7700',
     searchApiKey: '2c746b4e6e56e6376588f4952d35c3348487346e3380fd9e12b725bb5c259707',
@@ -15,4 +16,6 @@ export default defineNuxtConfig({
     serverSideUsage: true,
     adminApiKey: 'bfcb4330385d222886e29fed1721b8107acdba8043a4bf0738eb6d10ccbb5dfb',
   },
+
+  compatibilityDate: '2024-07-06',
 })
