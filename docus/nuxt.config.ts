c9ee7938368dcf3d8f82079e7f741fa75b8bd93c
xlanex6 <alexduval71@gmail.com>
2025-10-30 12:22:51 +0100

    chore: remove unused configuration and assets from docus template

--- docus/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
-
-  modules: [
-    // https://github.com/nuxt/devtools
-    '@nuxt/devtools',
-  ],
-})
